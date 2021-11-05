64aa27ab575f6f9022981db4891ffde946922f01
israel sodano <israel.almeida@xpi.com.br>
2021-11-05 12:52:26 -0300

    adicionando pontos sujos na base

---
+++ src/DPA.Sapewin.Domain/Entities/Appointment.cs
@@ -0,0 +1,32 @@
+using System;
+using DPA.Sapewin.Repository;
+
+namespace DPA.Sapewin.Domain.Entities
+{
+    public class Appointment : Entity
+    {       
+        public string Nfr { get; set; }
+
+        public int Nsr { get; set; }
+
+        public string UniqueAppointmentKey { get; set; }
+
+        public Guid EmployeeId { get; set; }
+
+        public Guid CompanyId { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public static implicit operator Appointment(DirtyNote obj){
+            DateTime d = Convert.ToDateTime(obj.Date.ToShortDateString() + " " + obj.Hour);
+
+            return obj == null ? null : new Appointment
+            {
+                Nfr = obj.Nfr,
+                Nsr = obj.Nsr,
+                UniqueAppointmentKey = obj.UniqueAppointmentKey,
+                Date = d,
+            };
+        }
+    }
+}
